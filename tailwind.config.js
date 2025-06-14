10b629ccc61ff5bd0b4dc13b0931110a7a4f2793
ninjangelo <lagdameoangelo@gmail.com>
2025-06-14 13:40:31 +0100

    added node_modules folder and overwritten the index.html file again

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/test.html",
+    "./src/*.html",
+    "./src/*.js",
+    "./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
