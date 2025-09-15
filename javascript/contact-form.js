68b7555dc6554cfcadc23054a89fe640efa08345
ninjangelo <lagdameoangelo@gmail.com>
2025-09-15 11:28:23 +0100

    testing the contact form (pt.1)

--- javascript/contact-form.js
+++ javascript/contact-form.js
@@ -4,14 +4,12 @@ emailjs.init({
     publicKey: "ETd_MNbvhSb8LUd8V",
 });
 
-window.onload = function() {
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        emailjs.sendForm('contact_service', 'contact_form', this)
-            .then(() => {
-                console.log('SUCCESS!');
-            }, (error) => {
-                console.log('FAILED...', error);
-            });
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    emailjs.sendForm('contact_service', 'contact_form', this)
+    .then(() => {
+        console.log('SUCCESS!');
+    }, (error) => {
+        console.log('FAILED...', error);
     });
-}
+});
