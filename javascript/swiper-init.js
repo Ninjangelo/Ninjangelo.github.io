285f54d3da8aa8d3e13d4d6901cf437080b91222
ninjangelo <lagdameoangelo@gmail.com>
2025-10-02 00:23:08 +0100

    swiper.js carousel re-formatting

--- javascript/swiper-init.js
+++ javascript/swiper-init.js
@@ -1,12 +1,25 @@
 var swiper = new Swiper(".mySwiper", {
-      cssMode: true,
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      centeredSlidesBounds: true,
+      centerInsufficientSlides: true,
+      spaceBetween: 3,
+      loop: true,
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 1,
+        modifier: 1,
+        slideShadows: true,
+      },
       navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
       },
       pagination: {
         el: ".swiper-pagination",
+        clickable: true,
       },
-      mousewheel: true,
-      keyboard: true,
-});
+    });
